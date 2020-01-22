1435442ab08bb85e4be45006c6f57c15e93ebc9f
Maiwenn KERVABON <mkervabo@student.42.fr>
2020-01-22 13:24:33 +0100

    add some partner to partner page add mofifie css

--- src/views/Partenaire.vue
+++ src/views/Partenaire.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="partenaire-fond">
     <div class="partenaire-body">
-      <div class="partenaire-icone partenaire-icone1">
+      <div class="partenaire-icone">
         <a href="http://fiiish.com/">
           <img class="partenaire-img" src="../assets/partner/fiiish.jpg" />
           <br />
           <div class="link">Fiiish</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone2">
+      <div class="partenaire-icone">
         <a href="http://www.suzukimarine.fr/">
           <img class="partenaire-img" src="../assets/partner/suzuki.png" />
           <br />
           <div class="link">Suzuki marine</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone1">
+      <div class="partenaire-icone">
         <a href="http://www.grand-pavois.com/">
           <img
             class="partenaire-img"
@@ -25,7 +25,7 @@
           <div class="link">Grand Pavois</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone2">
+      <div class="partenaire-icone">
         <a href="https://www.salon-peche-mer.com/">
           <img
             class="partenaire-img"
@@ -35,7 +35,7 @@
           <div class="link">Salon de la Pêche en Mer</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone1">
+      <div class="partenaire-icone">
         <a>
           <img
             class="partenaire-img"
@@ -45,28 +45,28 @@
           <div class="link">Pêche en Mer</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone2">
+      <div class="partenaire-icone">
         <a href="http://www.spiderwire.com/">
           <img class="partenaire-img" src="../assets/partner/spider_wire.png" />
           <br />
           <div class="link">Spider Wire</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone1">
-        <a href="http://www.pennfishing.com/penn-reels/">
-          <img class="partenaire-img" src="../assets/partner/Penn.jpg" />
+      <div class="partenaire-icone">
+        <a href="http://acp-atlantique.com/">
+          <img class="partenaire-img" src="../assets/partner/ACP.png" />
           <br />
-          <div class="link">Penn</div>
+          <div class="link">ACP Cabinet de recrutement</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone2">
-        <a>
-          <img class="partenaire-img" src="../assets/partner/power_pro.jpg" />
+      <div class="partenaire-icone">
+        <a href="https://www.kerfil.com/">
+          <img class="partenaire-img" src="../assets/partner/kerfil.jpg" />
           <br />
-          <div class="link">Power Pro</div>
+          <div class="link">Kerfil</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone1">
+      <div class="partenaire-icone">
         <a
           href="http://www.mediterranee.creditmaritime.fr/mer-assur-1264/id-menu-1046.html"
         >
@@ -75,21 +75,21 @@
           <div class="link">Mer Assur</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone2">
-        <a>
+      <div class="partenaire-icone">
+        <a href="https://www.mc-technologies.fr/">
           <img class="partenaire-img" src="../assets/partner/mc_tech.png" />
           <br />
           <div class="link">M.C Technologies</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone1">
+      <div class="partenaire-icone">
         <a href="https://delalande-peche.fr/">
           <img class="partenaire-img" src="../assets/partner/delalande.jpg" />
           <br />
           <div class="link">Delalande</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone2">
+      <div class="partenaire-icone">
         <a href="https://www.ultimate-fishing.net/">
           <img
             class="partenaire-img"
@@ -99,11 +99,94 @@
           <div class="link">Ultimate Fishing</div>
         </a>
       </div>
-      <div class="partenaire-icone partenaire-icone1">
+      <div class="partenaire-icone">
         <a href="http://www.besse.fr/">
           <img class="partenaire-img" src="../assets/partner/Besse.gif" />
           <br />
           <div class="link">Bessé</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a href="https://www.aon.com/home/index.html#">
+          <img class="partenaire-img" src="../assets/partner/Aon.svg" />
+          <br />
+          <div class="link">Aon</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a
+          href="https://www.beneteau.com/fr/beneteau-evenement-page-associee/participer-au-barracuda-tour"
+        >
+          <img
+            class="partenaire-img"
+            src="../assets/partner/barracuda-tour.png"
+          />
+          <br />
+          <div class="link">Barracuda Tour</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a>
+          <img class="partenaire-img" src="../assets/partner/tenryu.jpg" />
+          <br />
+          <div class="link">Tenryu</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a href="https://www.rodhouse.fr/fr/">
+          <img class="partenaire-img" src="../assets/partner/rodhouse.jpg" />
+          <br />
+          <div class="link">Rodhouse</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a href="https://www.alre-peche.com/">
+          <img class="partenaire-img" src="../assets/partner/alre-peche.jpg" />
+          <br />
+          <div class="link">Alré Pêche et Chasse</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a href="http://www.lepasco.com/">
+          <img class="partenaire-img" src="../assets/partner/le-pasco.png" />
+          <br />
+          <div class="link">Le Pasco</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a
+          href="https://fr-fr.facebook.com/pages/category/Media/RonProduction-134411313302276//"
+        >
+          <img
+            class="partenaire-img"
+            src="../assets/partner/ron-production.jpg"
+          />
+          <br />
+          <div class="link">Ron production</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a href="http://work.atm-communication.com/">
+          <img class="partenaire-img" src="../assets/partner/Atm-com.png" />
+          <br />
+          <div class="link">Atm Communication</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a>
+          <img
+            class="partenaire-img"
+            src="../assets/partner/Cap-Marine-Assurances.gif"
+          />
+          <br />
+          <div class="link">Cap Marine Assurances</div>
+        </a>
+      </div>
+      <div class="partenaire-icone">
+        <a>
+          <img class="partenaire-img" src="../assets/partner/tica.jpg" />
+          <br />
+          <div class="link">Tica</div>
         </a>
       </div>
     </div>
@@ -126,19 +209,10 @@
 }
 
 .partenaire-icone {
+  background-color: rgba(242, 242, 242, 0.8);
   padding: 5px;
   width: 300px;
   margin: 5px;
-}
-
-.partenaire-icone1 {
-  background-color: #f2f2f2;
-  color: #3a3a3a;
-}
-
-.partenaire-icone2 {
-  background-color: #3a3a3a;
-  color: #f2f2f2;
 }
 
 .partenaire-img {
