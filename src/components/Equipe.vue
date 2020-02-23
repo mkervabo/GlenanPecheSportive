75a9120643784628bcec2826dda3c6c881d2ab00
Maiwenn KERVABON <mkervabo@student.42.fr>
2020-02-23 17:08:58 +0100

    start annonce css

---
+++ src/components/Equipe.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="equipe">
+    <span class="equipe_number font">{{ number }}</span>
+    <span class="equipe_name white">{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["number", "name"]
+};
+</script>
+
+<style>
+.equipe {
+  margin: 0;
+}
+
+.equipe_number {
+  border: 1px solid #f2f2f2;
+}
+
+.equipe_name {
+  border-left: 1px solid #f2f2f2;
+}
+</style>
---
+++ src/views/Annonce.vue
@@ -0,0 +1,49 @@
+<template>
+  <main id="#annonce">
+    <div class="annonce_info">
+      <h2 class="subscription-title orange">A lire</h2>
+      <p class="annonce-content dark-olive">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut felis risus,
+        sagittis eget quam id, varius ultricies mi. Quisque ac nisl fermentum,
+        viverra nunc nec, egestas diam. Nam nec diam sit amet mi vestibulum
+        faucibus ut et metus. Nullam in mauris blandit, euismod nibh vel,
+        maximus turpis. Donec in nunc augue. Maecenas id ex neque. Donec
+        fermentum, nulla tincidunt sagittis congue, ligula massa vulputate
+        velit, sed vestibulum neque lectus a nulla.
+      </p>
+    </div>
+  </main>
+</template>
+
+<style>
+#annonce {
+  height: 100%;
+  background: #65aee2;
+  font: caption;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.annonce-content {
+  font: caption;
+  margin-left: 10px;
+  margin-right: 10px;
+  text-align: justify;
+}
+
+.annonce_info {
+  max-width: 900px;
+  padding: 10px;
+  margin: 10px auto;
+  background: #f2f2f2;
+  text-align: left;
+}
+
+.annonce_equipes {
+  max-width: 900px;
+  padding: 10px;
+  margin: 10px auto;
+  background: #3a3a3a;
+}
+</style>
