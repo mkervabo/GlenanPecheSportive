1dee7b1e8db854dda32aa15982e3b5f5a3cce8f0
mkervabo <mkervabo@student.42.fr>
2022-02-26 19:03:04 +0100

    send right information to the right spreadsheet

--- functions/subscription.js
+++ functions/subscription.js
@@ -13,8 +13,12 @@ const sheets = google.sheets({ version: "v4", auth });
 exports.handler = async event => {
   const body = JSON.parse(event.body);
 
+  const womenNum =
+    Number(body.mousse.kind === "F") + Number(body.patron.kind === "F");
+
   await sheets.spreadsheets.values.append({
     spreadsheetId: "140_Q_ZUeG7TfmV5_yfShMWMUvGZ_F1ue8dKF7w3d2v0",
+    sheetId: "1708966941",
     range: "A1",
     valueInputOption: "RAW",
     requestBody: {
@@ -23,9 +27,17 @@ exports.handler = async event => {
           body.equipage,
           `${body.patron.nom} ${body.patron.prenom}`,
           `${body.mousse.nom} ${body.mousse.prenom}`,
-          `${body.patron.email}\n${body.patron.portable}\n${body.patron.fixe}`,
-          `${body.mousse.email}\n${body.mousse.portable}\n${body.mousse.fixe}`,
+          `${body.patron.email}\n${body.patron.portable}`,
+          `${body.mousse.email}\n${body.mousse.portable}`,
           `site`,
+          `${130 + body.repas * 20}`,
+          `${body.repas}`,
+          "",
+          body.patron.t_shirt,
+          body.mousse.t_shirt,
+          (body.mousse.minor ? "JUNIOR " : "") +
+            ["", "MIXTE", "FEMININE"][womenNum],
+          `${womenNum}`,
           body.moteur,
           body.longueur,
           JSON.stringify(body)
